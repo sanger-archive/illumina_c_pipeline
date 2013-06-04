9d9684a24c7a42646ce7bbd2dd4962b20e3cb32e
James Glover <jg16@sanger.ac.uk>
2013-06-04 09:25:06 +0100

    Strip out unused presenters

--- app/models/forms/auto_pooling_form.rb
+++
@@ -1,9 +0,0 @@
-module Forms
-  class AutoPoolingForm < CreationForm
-    include Forms::Form::NoCustomPage
-
-    write_inheritable_attribute :page, 'auto_pooling'
-    write_inheritable_attribute :attributes, [:api, :user_uuid, :purpose_uuid, :parent_uuid, :transfer_template_uuid]
-    write_inheritable_attribute :default_transfer_template_uuid, Settings.transfer_templates['Pool wells based on submission']
-  end
-end
