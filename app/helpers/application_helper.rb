2c95de2583eb0d01c0c51f3590e59ae72130e2c4
James Glover <james.glover@sanger.ac.uk>
2015-12-09 14:35:18 +0000

    Input environenment assistance from IHTP

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,38 @@
 module ApplicationHelper
 
+  module DeploymentInfo
+
+    begin
+      require './lib/deployed_version'
+    rescue LoadError
+        module Deployed
+          VERSION_ID = 'LOCAL'
+          VERSION_STRING = "Generic App LOCAL [#{ENV['RACK_ENV']}]"
+        end
+    end
+
+    def version_information
+      # Provides a quick means of checking the deployed version
+      Deployed::VERSION_STRING
+    end
+  end
+  include DeploymentInfo
+
+
   def api
     Sequencescape::Api.new(IlluminaCPipeline::Application.config.api_connection_options)
+  end
+
+  def environment
+    Rails.env
+  end
+
+  def non_production_class
+    Rails.env != 'production' ? 'nonproduction' : ''
+  end
+
+  def custom_theme
+    yield 'nonproduction' unless Rails.env == 'production'
   end
 
 end
