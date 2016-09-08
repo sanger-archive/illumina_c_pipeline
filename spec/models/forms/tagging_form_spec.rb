c2863dbb81eee006b24c59f81af46a2207442dbf
James Glover <james.glover@sanger.ac.uk>
2016-09-08 18:57:39 +0100

    Add tests for key view requirements

--- spec/models/forms/tagging_form_spec.rb
+++ spec/models/forms/tagging_form_spec.rb
@@ -9,6 +9,8 @@ describe Forms::TaggingForm do
   # We retrieve the wells as part of this.
   stub_request_and_response('ilc-stock-plate-wells')
 
+  let(:wells_in_column_order) { (1..12).map {|c| ('A'..'H').map {|r| "#{r}#{c}" }}.flatten }
+
   context "On new" do
     let(:tagging_form) {
       Forms::TaggingForm.new(
@@ -17,6 +19,16 @@ describe Forms::TaggingForm do
         :api       => api
       )
     }
+
+    # These values all describe the returned json.
+    # They are used to prevent magic numbers from appearing in the specs
+    let(:plate_size) { 96 }
+    let(:occupied_wells) { 30 }
+    let(:pool_size) { 15 }
+    let(:largest_tag_group) { 120 }
+
+    let(:maximum_tag_offset)  { largest_tag_group - occupied_wells }
+    let(:maximum_well_offset) { plate_size - occupied_wells + 1 }
 
     it 'can be created' do
       expect(tagging_form).to be_a Forms::TaggingForm
@@ -29,11 +41,31 @@ describe Forms::TaggingForm do
       end
     end
 
-    # context 'with parent mocks' do
-    #   it 'describes the parent barcode' do
-    #     expect(presenter.labware.barcode.ean13).to eq('ilc-stock-plate-barcode')
-    #   end
-    # end
+    it 'describes the parent barcode' do
+      expect(tagging_form.labware.barcode.ean13).to eq('1220427444877')
+    end
+
+    it 'describes the parent uuid' do
+      expect(tagging_form.parent_uuid).to eq('ilc-stock-plate-uuid')
+    end
+
+    it 'describes the purpose uuid' do
+      expect(tagging_form.purpose_uuid).to eq('ilc-al-libs-tagged-uuid')
+    end
+
+    context 'with tag groups available' do
+      stub_request_and_response('tag-groups')
+      it 'describes the available tag groups' do
+        expect(tagging_form.tag_groups_with_uuid).to eq([["Suitably big group", "tag-group-c-uuid"]])
+      end
+      it 'describes the tag_range' do
+        expect(tagging_form.tag_range).to eq((0..maximum_tag_offset).map {|i| [i+1,i] } )
+      end
+    end
+
+    it 'describes the offsets' do
+      expect(tagging_form.offsets).to eq(wells_in_column_order.slice(0,maximum_well_offset).each_with_index.map {|w,i| [w,i]} )
+    end
 
   end
 
