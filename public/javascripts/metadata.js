c640c31c37deae40cb87faaa36d8cb26c2ebf026
Yana Proskurina <yana.proskurina@sanger.ac.uk>
2016-11-03 09:05:30 +0000

    ss534_add_metadata_to_labware minor changes

--- public/javascripts/metadata.js
+++ public/javascripts/metadata.js
@@ -9,45 +9,47 @@ $(document).ready(function() {
           .attr("id", "metadatum" +  id)
           .on('click', '.remove_metadatum', remove)
           .find('input').attr('required', 'true');
-
-      $('#metadatum' +  id).find('select')
+      $('#metadatum' +  id)
+          .on('change', 'select', checkDupKeys)
+          .find('select')
           .attr('id', "metadata__key__" + id)
       $('#metadata__key__'+id).selectmenu();
 
+      checkDupKeys()
       id++
-
   };
 
   function remove(){
       $(this).parent().parent().remove()
   };
 
-  function keysDuplicated(){
+  function checkDupKeys(){
       var stored  =   [];
       var inputs  =   $('select[id^="metadata__key__"');
-      var keysDup = false;
+      var dupKeys = false;
 
       $.each(inputs,function(k,v){
           var getVal  =   $(v).val();
           if(stored.indexOf(getVal) != -1){
               $(v).parent().css('border-color','red');
-              keysDup = true;
+              dupKeys = true;
           }else{
               $(v).parent().css('border-color','#aaa');
               stored.push($(v).val());
           };
       });
-      return keysDup
+      return dupKeys
   };
 
   $('#metadata-form').submit(function(){
-      var keysDup = keysDuplicated();
-      if (keysDup) {
+      var dupKeys = checkDupKeys();
+      if (dupKeys) {
           return false
       };
   });
 
   $(".add_metadatum").on("click", add);
   $(".remove_metadatum").on("click", remove);
+  $("select").on("change", checkDupKeys);
 
 });
