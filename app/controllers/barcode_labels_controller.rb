6720be955a2d8cf0ab67004bef1127196cc38602
James Glover <james.glover@sanger.ac.uk>
2019-02-14 10:05:56 +0000

    Quick switch to Print My Barcode
    This is hopefully the final release of Generic Lims, made a few
    days after we thought we could kill it. The replacement pipleine
    in LImber is almost complete, which should render this redundant.

--- app/controllers/barcode_labels_controller.rb
+++ app/controllers/barcode_labels_controller.rb
@@ -1,41 +1,40 @@
+require './lib/pmb_wrapper'
+
 class BarcodeLabelsController < ApplicationController
   before_filter :initialize_printer_and_barcode_service
-  def initialize_printer_and_barcode_service
-    raise StandardError, "No printer specified!" if params[:printer].blank?
-
-    @printer = api.barcode_printer.find(params[:printer])
-    @service = Sanger::Barcode::Printing::Service.new(@printer.service.url)
-    @copies = params[:number].to_i
-  end
-  private :initialize_printer_and_barcode_service
-
-  # Creates a label
-  def create_label(details)
-    Sanger::Barcode::Printing::Label.new(details)
-  end
-  private :create_label
-
-  # Does the actual printing of the labels passed
-  def print(labels)
-    @service.print_labels(Array(labels)*@copies, @printer.name, @printer.type.layout)
-  end
-  private :print
-
   # Handles printing a single label
   def individual
-    print(create_label(params[:label]))
-    redirect_to(params[:redirect_to], :notice => "Barcode printed to #{@printer.name}")
+    if print([params[:label]])
+      redirect_to(params[:redirect_to], :notice => "Barcode printed to #{@printer.name}")
+    else
+      redirect_to(params[:redirect_to], :error => "Barcode failed to print to #{@printer.name}")
+    end
   end
-
-  before_filter :convert_labels_to_array, :only => :multiple
-  def convert_labels_to_array
-    params[:labels] = params.fetch(:labels, []).map { |_, v| v }
-  end
-  private :convert_labels_to_array
 
   # Handles printing multiple labels
   def multiple
-    print(params[:labels].map(&method(:create_label)))
-    redirect_to(params[:redirect_to], :notice => "#{params[:labels].size} barcodes printed to #{@printer.try(:name)}")
+    if print(params[:labels].values)
+      redirect_to(params[:redirect_to], :notice => "#{params[:labels].size} barcodes printed to #{@printer.try(:name)}")
+    else
+      redirect_to(params[:redirect_to], :error => "Barcodes failed to print to #{@printer.name}")
+    end
+  end
+
+  private
+
+  def initialize_printer_and_barcode_service
+    raise StandardError, "No printer specified!" if params[:printer].blank?
+    @printer = api.barcode_printer.find(params[:printer])
+    @copies = params[:number].to_i
+  end
+
+  # Does the actual printing of the labels passed
+  def print(labels)
+    PmbWrapper.new(
+      IlluminaCPipeline::Application.config.pmb_url,
+      @printer.name,
+      @printer.type.layout,
+      labels*@copies
+    ).print_label
   end
 end
